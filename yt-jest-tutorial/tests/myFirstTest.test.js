4c137ab56a3d1c0513148975892d90dac0608696
tirth shah <tirth.s@simformsolutions.com>
2024-01-01 19:07:37 +0530

    feat: check for null

--- yt-jest-tutorial/tests/myFirstTest.test.js
+++ yt-jest-tutorial/tests/myFirstTest.test.js
@@ -11,3 +11,15 @@ describe('Number Operations', () => {
         expect(a + b).not.toBe(10);
     });
 });
+
+describe('Testing other matcher methods', () => {
+    test('Testing that a variable is undefined', () => {
+        let number = undefined;
+
+        expect(number).not.toBeDefined();
+        expect(number).toBeUndefined();
+        expect(number).not.toBeNull();
+        expect(number).toBeFalsy();
+        expect(number).not.toBeTruthy();
+    });
+});
